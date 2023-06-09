84b2c72180e622b7d5f58140129f4bd64a2c7476
Lionel Herrero Battista <lionherrero@gmail.com>
2023-06-09 18:50:18 -0300

    Finished exercise of section 4.

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -10,6 +10,19 @@ namespace CSharpIntermediate
     {
         static void Main(string[] args)
         {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            // output should be 
+            // 3
+            // 2
+            // 1
         }
     }
 }
---
+++ CSharpIntermediate/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharpIntermediate
+{
+    public class Stack
+    {
+        //arrayList to store?
+
+        private readonly ArrayList _stack;
+
+        public Stack()
+        {
+            _stack = new ArrayList();
+        }
+
+        // three methods
+
+        public void Push(object obj)
+        {
+            // stores the given obj on top
+            // obj null case
+            if (obj == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            object result;
+            int count = _stack.Count;
+            // removes the object on top
+            // case empty stack
+            if (count == 0)
+                throw new InvalidOperationException();
+
+            result = _stack[count - 1];
+
+            _stack.RemoveAt(count - 1);
+
+            return result;
+        }
+
+        public void Clear()
+        {
+            // removes all objects from stack
+            _stack.Clear();
+        }
+    }
+}
