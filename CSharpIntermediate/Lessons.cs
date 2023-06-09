89711ac1f88dfbf6121c95f7076f5789a70c5728
Lionel Herrero Battista <lionherrero@gmail.com>
2023-06-09 18:32:34 -0300

    Finished section 4: inheritance.

---
+++ CSharpIntermediate/Lessons.cs
@@ -0,0 +1,20 @@
+﻿using Amazon;
+
+namespace CSharpIntermediate
+{
+    internal partial class Program
+    {
+        public class Lessons
+        {
+            public static void LessonOne()
+            {
+                var customer = new Customer();
+            }
+
+            public static void LessonTwo() 
+            {
+                var car = new Car("XYZ1234");
+            }
+        }
+    }
+}
--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharpIntermediate
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
+            //Lessons.LessonOne(); // access modifiers
+            Lessons.LessonTwo();
+
         }
     }
 }
