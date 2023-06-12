3a505cb0d48fa871a518ecf378d24e798e68d2f0
Lionel Herrero Battista <lionherrero@gmail.com>
2023-06-12 13:16:50 -0300

    Started section 6 - interfaces.

--- CSharpIntermediate/Program.cs
+++ CSharpIntermediate/Program.cs
@@ -10,6 +10,13 @@ namespace CSharpIntermediate
     {
         static void Main(string[] args)
         {
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order
+            {
+                DatePlaced = DateTime.Now,
+                TotalPrice = 100f
+            };
+            orderProcessor.Process(order);
         }
     }
 }
